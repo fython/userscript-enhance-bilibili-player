3153a76f19393dbda9705c6fcd4d1299bde17d6e
Siubeng Fung <fython@163.com>
2020-03-24 10:43:17 +0000

    deploy: 4a0c7fc119cb771cb3834a94a5018126ebbcda79

---
+++ enhance-biliplayer.meta.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name 哔哩哔哩播放器增强
+// @version 0.1.7
+// @author Siubeng (fython)
+// @description Enhance Bilibili Player experience
+// @homepage https://github.com/fython/userscript-enhance-bilibili-player
+// @supportURL https://github.com/fython/userscript-enhance-bilibili-player/issues
+// @match *://www.bilibili.com/video/*
+// @match *://localhost*
+// @grant GM_setValue
+// @grant GM_getValue
+// @grant GM_addValueChangeListener
+// @grant GM_openInTab
+// ==/UserScript==
